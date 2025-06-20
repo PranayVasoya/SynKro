4e020f94c6551da8ddd461cca2daf89b6bee1e2f
devvratsaini <devvratsaini4@gmail.com>
2025-06-20 18:55:19 +0530

    Added expiry date in saved cookie for re-establishing the user's session

--- src/app/api/users/signin/route.ts
+++ src/app/api/users/signin/route.ts
@@ -10,21 +10,36 @@ export async function POST(request: NextRequest) {
     const { email, password } = await request.json();
     const user = await User.findOne({ email });
     if (!user) {
-      return NextResponse.json({ error: "User does not exist" }, { status: 400 });
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
     }
     const validPassword = await bcryptjs.compare(password, user.password);
     if (!validPassword) {
-      return NextResponse.json({ error: "Invalid credentials" }, { status: 400 });
+      return NextResponse.json(
+        { error: "Invalid credentials" },
+        { status: 400 }
+      );
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || "your-secret-key", {
-      expiresIn: "1d",
-    });
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET || "your-secret-key",
+      {
+        expiresIn: "1d",
+      }
+    );
     const response = NextResponse.json({
       message: "Logged In Success",
       success: true,
       data: { profileComplete: user.profileComplete },
     });
-    response.cookies.set("token", token, { httpOnly: true, path: "/" });
+
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+    const expiryDate = new Date(Date.now() + oneDayInMilliseconds);
+
+    response.cookies.set("token", token, { httpOnly: true, path: "/", expires: expiryDate, });
     return response;
   } catch (error: unknown) {
     console.error("Signin: Error:", error);
